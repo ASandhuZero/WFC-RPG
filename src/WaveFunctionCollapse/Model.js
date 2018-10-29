563e419e8a527b33b17ef46a1a32a977d04eaf7f
12435554+ASandhuZero@users.noreply.github.com <12435554+ASandhuZero@users.noreply.github.com>
2018-10-28 17:06:04 -0700

    Testing out WFC and seeing the lovely bugs that come up

--- src/WaveFunctionCollapse/Model.js
+++ src/WaveFunctionCollapse/Model.js
@@ -1,7 +1,7 @@
 export class Model {
     constructor(width, height) {
         this.wave;
-        this.propagtor;
+        this.propagator;
         this.compatible;
         this.observed;
 
@@ -11,9 +11,8 @@ export class Model {
         this.random;
         this.width = width;
         this.height = height;
-        this.tiles = [0];
-
-        this.weights = [0.1];
+        this.tiles;
+        this.weights; 
         this.log_weights;
 
         this.summed_weights;
@@ -33,18 +32,17 @@ export class Model {
         this.compatible = new Array(init_array_length);
 
         for (let i = 0; i < init_array_length; i++) {
-            this.wave[i] = new Array(this.tiles.length);
+            this.wave[i] = new Array(this.tiles.length).fill(true);
             this.compatible[i] = new Array(this.tiles.length);
-
+            
             for (let t = 0; t < this.tiles.length; t++) {
                 this.compatible[i][t] = new Array(4);
             }
         } 
-
         this.log_weights = new Array(this.tiles.length);
         this.summed_weights = 0;
         this.summed_log_weights = 0;
-
+        
         for (let t = 0; t < this.tiles.length; t++) {
             this.log_weights[t] = this.weights[t] * Math.log(this.weights[t]);
             this.summed_weights += this.weights[t];
@@ -52,10 +50,9 @@ export class Model {
         }
 
         this.starting_entropy = Math.log(this.summed_log_weights) - this.summed_log_weights / this.summed_weights;
-
         this.sums_of_ones = new Array(init_array_length);
-        this.summed_weights = new Array(init_array_length);
-        this.summed_log_weights = new Array(init_array_length);
+        this.sums_of_weights = new Array(init_array_length);
+        this.sums_of_log_weights = new Array(init_array_length);
         this.entropies = new Array(init_array_length);
     }
 
@@ -95,7 +92,7 @@ export class Model {
 
         let distribution = new Array(this.tiles.length);
         for (let t = 0; t < this.tiles.length; t++) {
-            distribution[t] = wave[argmin][t] ? weights[t] : 0;
+            distribution[t] = this.wave[argmin][t] ? this.weights[t] : 0;
         }
         // let r = distribution.random
         let r = distribution[Math.floor(Math.random()*distribution.length)];
@@ -116,7 +113,7 @@ export class Model {
             let e1 = this.stack[this.stacksize - 1]; //element 1
             this.stacksize = this.stacksize - 1;
 
-            let i1 = e1.Item1; // Item 1
+            let i1 = e1[0]; // Item 1
             let x1 = i1 % this.width;
             let y1 = i1 % this.height;
 
@@ -143,9 +140,9 @@ export class Model {
                 }
 
                 let i2 = x2 + y2 * this.width; // Item 2
-                let p = this.propagtor[d][e1.Item2];
+                debugger;
+                let p = this.propagator[d][e1[1]];
                 let compat = this.compatible[i2];
-
                 for (let l = 0; l < p.length; l++) {
                     let t2 = p[l] // tile of some sort
                     let comp = compat[t2]; // all compatible tiles of t2?
@@ -168,33 +165,37 @@ export class Model {
         this.random = Math.random() // IS NOT SEEDED
 
         for (let l = 0; l < limit || limit == 0; l++) {
-            let result = Observe();
+            let result = this.Observe();
+            
             if (result != null) {
                 return result;
             }
             this.Propagate();
+            // console.log(this.wave);
         }
         return true;
     }
 
-    Ban(i, t) { // i is item, t is tile
-        this.wave[i][t] = false;
+    Ban(item, tile) {
+        this.wave[item][tile] = false;
+        console.log(this.wave[item], item, tile);
+        
 
         let comp = this.compatible[item][tile];
         for (let d = 0; d < 4; d++) {
             comp[d] = 0;
-            this.stack[this.stacksize] = {};
+            this.stack[this.stacksize] = [item, tile];
             this.stacksize++;
 
-            let sum = this.sums_of_weights[i];
-            this.entropies[i] += this.sums_of_log_weights[i] / sum - Math.log(sum);
+            let sum = this.sums_of_weights[item];
+            this.entropies[item] += this.sums_of_log_weights[item] / sum - Math.log(sum);
 
-            this.sums_of_ones[i] -= 1;
-            this.sums_of_weights[i] -= this.weights[t];
-            this.sums_of_log_weights[i] -= this.log_weights[t];
+            this.sums_of_ones[item] -= 1;
+            this.sums_of_weights[item] -= this.weights[tile];
+            this.sums_of_log_weights[item] -= this.log_weights[tile];
 
-            sum = sums_of_weights[i];
-            this.entropies[i] -= this.sums_of_log_weights[i] / sum - Math.log(sum);
+            sum = this. sums_of_weights[item];
+            this.entropies[item] -= this.sums_of_log_weights[item] / sum - Math.log(sum);
         }
     }
 
@@ -204,18 +205,18 @@ export class Model {
             for (let t = 0; t < this.tiles.length; t++) {
                 this.wave[i][t] = true;
                 for (let d = 0; d < 4; d++) {
-                    debugger;
-                    this.compatible[i][t][d] = this.propagtor[opposite[d]][t].length;
+                    this.compatible[i][t][d] = this.propagator[opposite[d]][t].length; // compatible is the compatible tiles of t. NOT t itself. Which is why opposite is involved.
                 }
-
+                
                 this.sums_of_ones[i] = this.weights.length;
                 this.sums_of_weights[i] = this.summed_weights;
                 this.sums_of_log_weights[i] = this.summed_log_weights;
                 this.entropies[i] = this.starting_entropy;
             }
         }
+        debugger;
     }
     OnBoundary(x,y) {
-        pass
+        pass;
     }
 }
