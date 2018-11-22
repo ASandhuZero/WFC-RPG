d14aab6e4c66ba4961aeda58692c0ce7023f2df1
ZEE CHEN <zeyuan.chen1126@gmail.com>
2018-11-21 20:07:20 -0800

    feat: add constraints.js

---
+++ src/Model/WaveFunctionCollapse/Constraints/Constraints.js
@@ -0,0 +1,11 @@
+export function Dependency() {
+    console.log("dependency")
+}
+
+export function Locality() {
+    console.log("locality")
+}
+
+export function Rotation() {
+    console.log("rotation")
+}
