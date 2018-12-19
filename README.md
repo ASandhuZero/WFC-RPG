b4016df823f05b644e483103e70ccefe5bffc921
unknown <zeyuan.chen1126@gmail.com>
2018-12-18 22:08:07 -0800

    feat: electronjs displays app on secondary monitor

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Run: browser-sync
+
+
+## Necessary Packages
+1. Node.js
+2. NPM
+3. Global install of JSPM
+`npm install jspm -g`
+`jspm install`
+`jspm update`
+4. <a href="https://electronjs.org/docs/tutorial/installation">electronjs</a>
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // Modules to control application life and create native browser window
+const electron = require('electron')
 const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -6,21 +7,32 @@ const {app, BrowserWindow} = require('electron')
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({fullscreen: true, fullscreenWindowTitle : true})
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  let displays = electron.screen.getAllDisplays()
+  let externalDisplay = displays.find((display) => {
+    return display.bounds.x !== 0 || display.bounds.y !== 0
   })
+  if (externalDisplay) {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+      x: externalDisplay.bounds.x + 50,
+      y: externalDisplay.bounds.y + 50,
+      fullscreen: true, 
+      fullscreenWindowTitle : true
+    })
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null
+    })
+  }
 }
 
 // This method will be called when Electron has finished
@@ -40,6 +52,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
+  
   if (mainWindow === null) {
     createWindow()
   }
