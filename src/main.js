c326e66822c8e8b0e5f3b5041fd7689067b7aed2
asandhuzero <12435554+ASandhuZero@users.noreply.github.com>
2021-07-28 23:21:40 -0700

    Lol left a test flag on. Whoops.

--- src/main.js
+++ src/main.js
@@ -100,7 +100,7 @@ let featureMapping = {
 // found :)
 let partial = null;
 let partialFlag = true;
-let testPaths = true;
+let testPaths = false;
 let strict = false;
 let banList = [19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37];
 let shouldGenerateNeighbors = true;
